d108eb8aaf349315c115253415227f4f7267dad0
Ejenie <baranova.evg@inbox.ru>
2024-12-24 17:29:36 +0300

    Новая версия

---
+++ Cheburashka_firmware/study/motors/platform_motors.h
@@ -0,0 +1,16 @@
+//left motor
+#define ML_EN 0
+#define ML_A 0
+#define ML_B 0
+#define ML_DIR 0
+
+#define ML_ENC_INT 0
+#define ML_ENC_PWM 0
+//right motor
+#define MR_EN 0
+#define MR_A 0
+#define MR_B 0
+#define MR_DIR 0
+
+#define MR_ENC_INT 0
+#define MR_ENC_PWM 0
